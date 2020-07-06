2244646e38b7f9921d698ac9c1920f13064818a5
DaniFascio <fascianidaniele@gmail.com>
2020-07-06 22:48:36 +0200

    db connection

---
+++ src/main/java/io/github/DaniFascio/DatabaseConnection.java
@@ -0,0 +1,38 @@
+package io.github.DaniFascio;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    public static final String URL = "jdbc:postgres://localhost:5432/db_officina";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "fdm3006";
+
+
+    private Connection conn;
+
+    public void connect() {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+            if (conn != null)
+                System.out.println("connected to the database");
+            else
+                System.out.println("failed to make connection");
+
+        }
+
+        catch (SQLException e) {
+            System.err.format("SQL state: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+
+    }
+}

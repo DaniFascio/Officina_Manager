3d9c1fef28130f160fc17b43ae77fd62f5f1160c
excale <escaccia@live.it>
2020-07-09 14:22:24 +0200

    aiut

--- src/main/java/io/github/DaniFascio/gui/LoginScene.java
+++ src/main/java/io/github/DaniFascio/gui/LoginScene.java
@@ -1,9 +1,12 @@
 package io.github.DaniFascio.gui;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 
 import java.io.IOException;
@@ -29,6 +32,17 @@ public class LoginScene {
 
 	private BorderPane getRootPane() {
 		return rootPane;
+	}
+
+	@FXML
+	private void closeWindow() {
+		Platform.exit();
+	}
+
+	@FXML
+	private void closeWindowEnter(KeyEvent event) {
+		if(event.getCode().equals(KeyCode.ENTER))
+			Platform.exit();
 	}
 
 }

bdc1e056a9ac04ffdbad6f34216e28214778da03
Excale <dev_excale@hotmail.com>
2020-07-12 12:43:48 +0200

    Reworked AutoAddDialog

--- src/main/java/io/github/DaniFascio/gui/controllers/ManagerScreen.java
+++ src/main/java/io/github/DaniFascio/gui/controllers/ManagerScreen.java
@@ -4,13 +4,10 @@ import io.github.DaniFascio.Auto;
 import io.github.DaniFascio.AutoDao;
 import io.github.DaniFascio.gui.Screen;
 import io.github.DaniFascio.gui.components.AutoCell;
-import javafx.collections.ListChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
@@ -23,12 +20,21 @@ public class ManagerScreen implements Screen {
 	private ListView<?> rightList;
 	@FXML
 	private ListView<Auto> leftList;
+
 	@FXML
 	private Label targaLabel;
 	@FXML
 	private Label modelloLabel;
 	@FXML
 	private Label kmLabel;
+	@FXML
+	private Label misuraGommeLabel;
+	@FXML
+	private Label tipoGommeLabel;
+	@FXML
+	private Label totaleLavorazioniLabel;
+	@FXML
+	private Label noteLabel;
 
 	private final Pane view;
 
@@ -54,13 +60,21 @@ public class ManagerScreen implements Screen {
 					targaLabel.setText(newValue.getTarga());
 					modelloLabel.setText(newValue.getModello());
 					kmLabel.setText(newValue.getKm().toString());
+					misuraGommeLabel.setText(newValue.getMisuraGomme());
+					tipoGommeLabel.setText(newValue.getTipoGomme());
+					totaleLavorazioniLabel.setText("0");
+					noteLabel.setText(newValue.getNote());
 				});
 	}
 
 	@FXML
 	private void onAdd(ActionEvent actionEvent) {
-		leftList.getItems()
-				.add(new Auto("XX000XX", "Fiat", 1000, "Boh", "Qualcosa", null));
+		Dialog<Auto> dialog = new AutoAddDialog();
+
+		dialog.getDialogPane()
+				.getButtonTypes()
+				.addAll(new ButtonType("Aggiungi", ButtonBar.ButtonData.APPLY), new ButtonType("Cancella", ButtonBar.ButtonData.CANCEL_CLOSE));
+		dialog.showAndWait();
 	}
 
 	@FXML
